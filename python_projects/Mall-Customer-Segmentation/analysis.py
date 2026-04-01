25f67c68c7a1ec3d2b0191c372600fed23c404c7
Tamim-Ayoub <260507739+Tamim-Ayoub@users.noreply.github.com>
2026-04-01 20:55:08 +0200

    Initial processing of the data

--- python_projects/Mall-Customer-Segmentation/analysis.py
+++ python_projects/Mall-Customer-Segmentation/analysis.py
@@ -6,4 +6,51 @@ import matplotlib.pyplot as plt
 
 import seaborn as sns
 
-kagglehub.dataset_download('bricevergnou/spotify-recommendation',force_download=True,output_dir='./Mall-Customer-Segmentation')
+
+# Download the Data
+
+#kagglehub.dataset_download("vjchoudhary7/customer-segmentation-tutorial-in-python",output_dir='./data',force_download=True)
+
+
+
+data = pd.read_csv("./data/Mall_Customers.csv")
+
+
+# Examine the data and its integrity (Making sure no missing values exist)
+print(data.head(10))
+print(data.info())
+print(data.shape)
+
+
+
+# Change Column names for easier processing
+
+data.columns = ["CustomerID","gender","age","annual_income","spending_score"]
+
+
+
+# Make sure no CustomerID value is repeated:
+
+print(data.value_counts("CustomerID"))
+
+# Sort the data accoring to the Spending Score in descending order.
+
+data_sorted = data.sort_values("spending_score",ascending=False)
+
+print(data_sorted.head())
+
+
+
+
+# Add a column to categorize the spending_score:
+
+conditions = [
+    data["spending_score"] <= 10,
+    (data["spending_score"] > 10) & (data['spending_score'] <= 25),
+    (data['spending_score'] > 25) & (data['spending_score'] <= 50),
+    (data['spending_score'] > 50) & (data['spending_score'] <= 75),
+    data['spending_score'] > 75
+]
+
+categorizes = ["window_shoppers",'budget_conscious','normal_spenders','big_spenders','extravagent_spenders']
+

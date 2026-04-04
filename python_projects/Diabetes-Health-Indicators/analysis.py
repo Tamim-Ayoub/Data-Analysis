7799121694b64013b94180fdf2077f94f27f0cfe
Tamim-Ayoub <260507739+Tamim-Ayoub@users.noreply.github.com>
2026-04-04 13:43:03 +0200

    Add a column for Diabetes-yes-no

--- python_projects/Diabetes-Health-Indicators/analysis.py
+++ python_projects/Diabetes-Health-Indicators/analysis.py
@@ -3,6 +3,7 @@ import kagglehub
 
 # Download the Data
 #kagglehub.dataset_download("alexteboul/diabetes-health-indicators-dataset",output_dir='./data',force_download=True)
+
 import pandas as pd
 
 import matplotlib.pyplot as plt
@@ -12,6 +13,27 @@ import seaborn as sns
 import numpy as np
 
 
+# Read the data
+data = pd.read_csv("/home/edward/repos/Data-Analysis/python_projects/Diabetes-Health-Indicators/data/diabetes_012_health_indicators_BRFSS2015.csv")
+
+
+"""Checking the integrity of the data"""
+
+print(data.head(),'\n')
+
+print(data.info(),'\n')
+
+print(data.shape,'\n')
+
+"""Integrity checked. No missing values detected"""
 
 
 
+""" Add a column to change the data types of Diabetes_012 from 0,1,2 to : Diabetic for having diabetes, 
+and Healthy for healthy people. This is useful for simplifying the analysis
+"""
+
+data["Diabetes_yes_no"] = data["Diabetes_012"].apply(lambda x: "Diabetic" if x > 0 else "Healthy")
+
+
+print(data.head())

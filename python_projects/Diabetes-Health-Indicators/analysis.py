018047aa289c8c3f8bcc2e3cbddea3b595a81816
Tamim-Ayoub <260507739+Tamim-Ayoub@users.noreply.github.com>
2026-04-04 13:24:14 +0200

    Add the data and read it into a DF

---
+++ python_projects/Diabetes-Health-Indicators/analysis.py
@@ -0,0 +1,17 @@
+import kagglehub 
+
+
+# Download the Data
+#kagglehub.dataset_download("alexteboul/diabetes-health-indicators-dataset",output_dir='./data',force_download=True)
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+
+import numpy as np
+
+
+
+
+
